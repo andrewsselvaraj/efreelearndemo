c0f9bad22257be07768ca1f86a40c85255a5c611
andrewsselvaraj <andrewsselvaraj@gmail.com>
2020-04-10 22:09:55 +0530

    Added Organisation

---
+++ spring-boot-jdbc-example/src/main/java/com/mkyong/AddOrganisation.java
@@ -0,0 +1,60 @@
+package com.mkyong;
+
+import static java.lang.System.exit;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.mkyong.dao.OrganisationRepository;
+import com.mkyong.dao.UserInfoRepository;
+import com.mkyong.model.UserInfo;
+
+@SpringBootApplication
+public class AddOrganisation implements CommandLineRunner {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    private OrganisationRepository organisationRepository;
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(AddOrganisation.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        System.out.println("DATASOURCE = " + dataSource);
+
+                SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+                Date date = new Date();
+                String id = ""+date.getTime();
+                String pk_SchoolId = "11111";
+                String password = "password";
+                String SchoolName = "aac";
+                String PrincipalName = "charles"; 
+                String SchoolAddress = "a";
+                String City = "city";
+                String State = "state";
+                String Country = "con";
+                String Pincode = "625777";
+                String Email = "andrew@yahoo.com";
+                String Website = "aklsdlajs";
+                String ContactNo = "9444104269	";
+                String createdby = "83094823";
+                String lastupdateed  = "098301";
+                String status =  "A";
+                organisationRepository.addOrganisation(   pk_SchoolId, password,    SchoolName,   PrincipalName,   SchoolAddress,   City,   State,  Country,  Pincode,  Email,  Website,   ContactNo,  createdby,   lastupdateed,  status);
+           
+      
+    }
+}
---
+++ spring-boot-jdbc-example/src/main/java/com/mkyong/dao/OrganisationRepository.java
@@ -0,0 +1,41 @@
+package com.mkyong.dao;
+
+import com.mkyong.model.UserInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public class OrganisationRepository {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public List<UserInfo> findAll() {
+
+        List<UserInfo> result = jdbcTemplate.query(
+                "SELECT pk_user_id, user_name, email_id, created_datettime FROM user_info",
+                (rs, rowNum) -> new UserInfo(rs.getInt("pk_user_id"),
+                        rs.getString("user_name"), rs.getString("email_id"), rs.getDate("created_datettime"))
+        );
+
+        return result;
+
+    }
+
+    public void addOrganisation(String pk_SchoolId, String password, String SchoolName, String PrincipalName, String SchoolAddress, String City, String State, String Country, String Pincode, String Email, String Website, String ContactNo, String createdby, String lastupdateed, String status) {
+    	
+    	String sql = "INSERT INTO `school_info` (`pk_SchoolId`, `password`, `SchoolName`, `PrincipalName`, `SchoolAddress`, `City`, `State`, `Country`, `Pincode`, `Email`, `Website`, `ContactNo`, `createdby`, `lastupdateed`, `status`) VALUES (";
+    	sql = sql + "'"+ pk_SchoolId +"','"+ password +"','"+ SchoolName +"','"+ PrincipalName +"','"+  SchoolAddress +"','"+ City +"','" + State +"','"+  Country +"','"+  Pincode +"','"+  Email +"','"+ Website +"','"+  ContactNo +"','"+  createdby +"','"+  lastupdateed +"','"+  status+"' )";
+    			
+    			
+
+        jdbcTemplate.update(sql);
+
+    }
+
+
+}
