a46e66b6960517be2e2a81dea58b2cf70e99072a
shiju <shiju@LAPTOP-QFQM3362>
2020-04-21 22:19:32 +0530

    DB Controller Added

--- spring-boot-jdbc-example/src/main/java/com/efreelearn/controller/SchoolInfoRestController.java
+++ spring-boot-jdbc-example/src/main/java/com/efreelearn/controller/SchoolInfoRestController.java
@@ -1,15 +1,39 @@
 package com.efreelearn.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.efreelearn.dao.OrganisationRepository;
+import com.efreelearn.model.UserInfo;
+
 @RestController
 public class SchoolInfoRestController {
+	
+	@Autowired
+	OrganisationRepository organisationRepository;
 	
 	@RequestMapping("/index")
 	public String getSchoolInfo(String id)
 	{
 		return "empty";
 	}
+	
+	@RequestMapping("/allSchoolInfo")
+	public List<UserInfo> getAllSchoolInfo()
+	{
+		List<UserInfo> userList=organisationRepository.findAll();
+		return userList;
+	}
+	
+	@RequestMapping("/apNew")
+    public  ArrayList<String> addtionalPath() {
+    	ArrayList<String> al = new ArrayList<String>();
+    	al.add("arg0");al.add("arg1");
+        return al;
+    }
 
 }
