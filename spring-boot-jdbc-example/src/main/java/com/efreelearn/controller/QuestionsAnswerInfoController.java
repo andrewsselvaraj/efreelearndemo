49b03d0ec869e54da08a5487d58458160ad2d7ee
shiju <shiju@LAPTOP-QFQM3362>
2020-05-02 14:10:07 +0530

    Display all questions

--- spring-boot-jdbc-example/src/main/java/com/efreelearn/controller/QuestionsAnswerInfoController.java
+++ spring-boot-jdbc-example/src/main/java/com/efreelearn/controller/QuestionsAnswerInfoController.java
@@ -5,9 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.efreelearn.dao.QuestionsRepository;
+import com.efreelearn.dao.QuestionAnswersRepository;
 import com.efreelearn.model.QuestionAnswers;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -15,15 +16,22 @@ import com.efreelearn.model.QuestionAnswers;
 public class QuestionsAnswerInfoController {
 	
 	@Autowired
-	QuestionsRepository questionsRepository;
+	QuestionAnswersRepository questionAnswersRepository;
 
 	
 
 	@CrossOrigin(origins = "*", allowedHeaders = "*")
-	@RequestMapping("/findAllQuestionwithAnswers")
+	@RequestMapping(value="/findAllQuestionwithAnswers", method =  RequestMethod.GET)
 	public List<QuestionAnswers> findAllQuestionwithAnswers()
 	{
-		List<QuestionAnswers> userList=questionsRepository.findAllQuestionwithAnswers("pk_SubjectId", "fk_SchoolId");
+		List<QuestionAnswers> userList=questionAnswersRepository.findAllQuestionwithAnswers("pk_SubjectId", "fk_SchoolId");
+		return userList;
+	}
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
+	@RequestMapping(value="/findAllQuestionwithAnswerforAll", method =  RequestMethod.GET)
+	public List<QuestionAnswers> findAllQuestionwithAnswerforAll()
+	{
+		List<QuestionAnswers> userList=questionAnswersRepository.findAllQuestionwithAnswerforAll("pk_SubjectId", "fk_SchoolId");
 		return userList;
 	}
 	
--- spring-boot-jdbc-example/src/main/java/com/efreelearn/dao/QuestionAnswersRepository.java
+++ spring-boot-jdbc-example/src/main/java/com/efreelearn/dao/QuestionAnswersRepository.java
@@ -20,7 +20,7 @@ import com.efreelearn.model.QuestionAnswers;
  
 
 @Repository
-public class QuestionsRepository {
+public class QuestionAnswersRepository {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -32,7 +32,7 @@ public class QuestionsRepository {
     	String sql = "SELECT q.QuestionName,q.pk_questionid,a.answer,a.correct_answer FROM question_info q , answer_info a ";
     	sql = sql + " where q.fk_SchoolId= a.fk_SchoolId and q.fk_SubjectId=a.fk_SubjectId and q.pk_questionid=a.fk_questionid ";
     	sql = sql + " and q.fk_SubjectId='" +pk_SubjectId+"' and q.fk_SchoolId='"+fk_SchoolId;
-    	jdbcTemplate.execute(sql);
+    	//jdbcTemplate.execute(sql);
         return jdbcTemplate.query(sql,
                 new ResultSetExtractor<List<Question>>() {
                     public List<Question> extractData(ResultSet rs) throws SQLException, DataAccessException {
@@ -63,11 +63,95 @@ public class QuestionsRepository {
 
                 });
     }
+    
+    public List<QuestionAnswers> findAllQuestionwithAnswerforAll(String pk_SubjectId,String fk_SchoolId) {
+    	
+    	String sql = "SELECT q.QuestionName,q.pk_questionid,a.answer,a.correct_answer FROM question_info q , answer_info a ";
+    	sql = sql + " where q.fk_SchoolId= a.fk_SchoolId and q.fk_SubjectId=a.fk_SubjectId and q.pk_questionid=a.fk_questionid order by q.pk_questionid";
+    	//sql = sql + " and q.fk_SubjectId='" +pk_SubjectId+"' and q.fk_SchoolId='"+fk_SchoolId;
+    	//sql = sql + " and q.fk_SubjectId='" +pk_SubjectId+"' and q.fk_SchoolId='"+fk_SchoolId;
+    	//jdbcTemplate.execute(sql);
+        return jdbcTemplate.query(sql,
+                new ResultSetExtractor<List<QuestionAnswers>>() {
+                    public List<QuestionAnswers> extractData(ResultSet rs) throws SQLException, DataAccessException {
+                    	List<QuestionAnswers> lstQA= new ArrayList<QuestionAnswers>();
+                    	 QuestionAnswers questionAnswers=  new QuestionAnswers();
+                    	
+                    	Question question = new Question();
+                    	//Answer answer = new Answer();
+                    	
+                    	
+                    	
+                        List<Answer> answerLst = new ArrayList<Answer>();
+                        String questionId = "";
+        
+                        int questionIdx = 0;
+                        int answerIdx = 0;
+                        int q=-1;
+                        int a=-1;
+                        while (rs.next()) {
+                        	
 
+                        	
+                            // first row or when order changes
+                            if (question == null || !questionId.equals(rs.getString("pk_questionid"))) {
+                            	question = new Question();
+                            	answerLst = new ArrayList<Answer>();
+                            	 System.out.println("question no"+q);
+                            	questionId = rs.getString("pk_questionid");
+                            	//currentQuestion = questionMapper.mapRow(rs, questionIdx++);
+                            	question.setPk_questionid(""+rs.getString("pk_questionid"));
+                            	question.setQuestionName(rs.getString("QuestionName"));
+                            	
+                            	//qaList.setQuestion(question);
+                              	 
+
+                            	
+                            	a = 0;
+                            	
+                            	
+                             	questionAnswers = new QuestionAnswers();                             
+                            	questionAnswers.setQuestion(question);
+                            	questionAnswers.setAnswers(answerLst);
+                            	if(q!=-1)
+                            	{
+                            		System.out.println(""+ q+""+rs.getString("QuestionName"));
+                            	lstQA.add(q, questionAnswers);
+                            	//questionAnswers.setAnswers(null);
+                            	}
+                            	q=q+1;
+
+                            }
+                            else
+                            {
+                            	a= a+1;
+                            	 System.out.println("answer no"+a);
+                            	 Answer answer = new Answer();
+                            	answer.setFk_questionid(""+questionId);
+                            	answer.setAnswer(rs.getString("answer"));
+                            	answer.setCorrect_answer(rs.getString("correct_answer"));
+                            	answerLst.add(answer);
+                            	//qaList.setAnswers(answerLst);
+
+                            }
+                            //lstQA.add(questionAnswers);
+                            
+               
+                        }
+                        
+                        return lstQA;
+                    }
+
+                });
+     
+    }
+
+  
     public List<QuestionAnswers> findAllQuestionwithAnswers(String pk_SubjectId,String fk_SchoolId) {
     	
     	List<QuestionAnswers> lstQA= new ArrayList<QuestionAnswers>();
     	QuestionAnswers qaList= new QuestionAnswers();
+    	QuestionAnswers qaList2= new QuestionAnswers();
     	Question question = new Question();
     	question.setPk_questionid("1");
     	question.setQuestionName("which is capital of delhi");
@@ -134,15 +218,22 @@ public class QuestionsRepository {
     	answers2.add(b2);
     	answers2.add(b3);    	
     	answers2.add(b4);
-    	qaList.setAnswers(answers2);
-    	qaList.setQuestion(question2);
+    	qaList2.setAnswers(answers2);
+    	qaList2.setQuestion(question2);
     	
     	
     	lstQA.add(qaList);
+    	lstQA.add(qaList2);
     	
     	return lstQA;
     	
     	
     } 
+    public static void main(String args[])
+    {
+    	QuestionAnswersRepository q = new QuestionAnswersRepository();
+    	List<QuestionAnswers> a= q.findAllQuestionwithAnswers("pk_SubjectId", "fk_SchoolId");
+    	a.forEach(System.out::println);
+    }
 }
 
