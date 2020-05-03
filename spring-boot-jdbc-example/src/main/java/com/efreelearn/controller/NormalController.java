6c066db17b901c17b39fa38bf1dc9618d1770601
shiju <shiju@LAPTOP-QFQM3362>
2020-05-03 12:28:41 +0530

    New JSPS

--- spring-boot-jdbc-example/src/main/java/com/efreelearn/controller/NormalController.java
+++ spring-boot-jdbc-example/src/main/java/com/efreelearn/controller/NormalController.java
@@ -10,8 +10,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class NormalController {
 @RequestMapping(value = "/add-todo", method = RequestMethod.GET)
 public String showAddTodoPage(ModelMap model) {
-    model.addAttribute("todo", "");
+	 model.addAttribute("todo", "");
     return "todo";
+
+}
+@RequestMapping(value = "/next", method = RequestMethod.GET)
+public String nextMethod(ModelMap model) {
+    model.addAttribute("message", "message");
+    model.addAttribute("todo", "");
+    return "next";
 
 }
 }
