9b5d31ba58fdf2872f0ab2d9efaee166f6acaa6e
shiju <shiju@LAPTOP-QFQM3362>
2020-05-05 10:43:51 +0530

    User Response

---
+++ spring-boot-jdbc-example/src/main/java/com/efreelearn/response/UserResponse.java
@@ -0,0 +1,34 @@
+package com.efreelearn.response;
+
+public class UserResponse {
+	String email;
+	String date;
+	String id;
+	String name;
+			public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+}
