787c7b2b84c8ac7ffae08738f0aa9c613bf620cf
shiju <shiju@LAPTOP-QFQM3362>
2020-05-05 10:38:07 +0530

    user info

---
+++ spring-boot-jdbc-example/src/main/java/com/efreelearn/unittest/QuestionandAnswerInfo.java
@@ -0,0 +1,21 @@
+package com.efreelearn.unittest;
+
+import org.springframework.web.client.RestTemplate;
+
+import com.efreelearn.response.UserResponse;
+
+public class QuestionandAnswerInfo {
+
+	public static void main(String[] args) {
+		String url = "https://spring-jdbc.cfapps.io/validateUserDummy?userName=shirly&password=shirly";
+		RestTemplate restTemplate = new RestTemplate();
+		UserResponse[] u = restTemplate.getForObject(url, UserResponse[].class);
+		System.out.println("aaa "+u[0].getEmail());
+
+	
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
